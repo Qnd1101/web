aea21c307f0fd2dc0d9e87295716e1a02d133ddb
MinJun <mj061101k@naver.com>
2024-04-29 16:36:42 +0900

    12_tab_memu & A-1 모의고사 html A,B,C 완성

---
+++ 12_탭메뉴/script/script.js
@@ -0,0 +1,30 @@
+$(function(){
+    $(".nav>ul>li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown(200)
+    });
+});
+
+$(function(){
+    $(".nav>ul>li").mouseleave(function(){
+        $(this).find(".submenu").stop().slideUp(200)
+    });
+});
+
+$(function(){
+    let tabBtn = $(".tab_btn>ul>li");
+    let tabCont = $(".tab_cont>div");
+
+    tabCont.hide().eq(0).show();
+    
+    tabBtn.click(function(e){
+        e.preventDefault();
+
+        let target = $(this);
+        let index = target.index();
+
+        tabBtn.removeClass("active");
+        target.addClass("active");
+        tabCont.css("display","none");
+        tabCont.eq(index).css("display","block");
+    });
+});
