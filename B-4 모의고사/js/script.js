f7c1165baa52da55a8da72c87fa0e78a3d5e20ad
MinJun <mj061101k@naver.com>
2024-06-21 21:40:36 +0900

    B-4 완성

--- B-4 모의고사/js/script.js
+++ B-4 모의고사/js/script.js
@@ -1,8 +1,25 @@
 $(function(){
-    $(".gnb").mouseover(function(){
+    $(".gnb li").mouseover(function(){
         $(".lnb").stop().slideDown();
+        $(".bg").stop().slideDown();
     });
-    $(".gnb").mouseleave(function(){
+    $(".gnb li").mouseleave(function(){
         $(".lnb").stop().slideUp();
+        $(".bg").stop().slideUp();
+    });
+
+    setInterval(function(){
+        $(".slide ul").animate({left:-1200}, 2000, function(){
+            $(".slide li").eq(0).appendTo(".slide ul");
+            $(".slide ul").css({left:0})
+        });
+    }, 2000);
+
+    $(".notice li").eq(0).click(function(){
+        $(".layer_bg").show();
+    });
+
+    $(".layer a").click(function(){
+        $(".layer_bg").hide();
     });
 });
